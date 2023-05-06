beb8e5884b6a0294c8f916b8f89c3177c45da570
Agnieszka <agnieszka.ter@gmail.com>
2023-05-06 15:03:11 +0200

    clean config

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+try:
+    from config.local_settings import SECRET_KEY
+except ModuleNotFoundError:
+    print('Secret Key nieznaleziony')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
